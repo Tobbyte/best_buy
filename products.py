79f669737e955f332d81a0235962d91f185c60e7
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-14 16:38:39 +0200

    use validator

--- products.py
+++ products.py
@@ -1,35 +1,25 @@
-# ruff: noqa: D100, D107, D101, D102,ERA001
-from typing import Any
+# ruff: noqa: D100, D107, D101, D102
+from typing import Any, ClassVar
+
+from fields_validator import validate
 
 
 class Product:
     ERR_INIT = "Wrong init params."
     ERR_OUTOFSTOCK = "Out of stock"
     ERR_BUY = "BUY err"
-    _FIELD_TYPES = {"name": str, "price": float | int, "quantity": int}
+    _EVALD_FIELDS: ClassVar[dict] = {
+        "name": str,
+        "price": float | int,
+        "quantity": int,
+    }
 
+    @validate(_EVALD_FIELDS)
     def __setattr__(self, name: str, value: Any) -> None:
-        """Validate."""
-        expected_type = self._FIELD_TYPES.get(name)
-        if expected_type and not isinstance(value, expected_type):
-            err_msg = f"{name} is not of type {expected_type}"
-            raise TypeError(err_msg)
+        """Set attribute with validation."""
         super().__setattr__(name, value)
 
-    # @classmethod
-    # def _is_valid(cls, sets: list[tuple[Any, (tuple | type)]]) -> bool:
-    #     for val, data_type in sets:
-    #         if not isinstance(val, (data_type,)):
-    #             err_msg = f"{val} is not of type {data_type}"
-    #             raise TypeError(err_msg)
-    #     return True
-
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        # Product._is_valid([
-        #     (name, str),
-        #     (price, (float, int)),
-        #     (quantity, int),
-        # ])
 
         self.name = name
         self.price = price
