df8c5a3f2eed60c901309a925355cad27443888a
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-16 10:40:15 +0200

    refactor as class

--- main.py
+++ main.py
@@ -25,153 +25,144 @@ from valid_tobbyte_module.valid_tobbyte.validator_fn import (
     validate_fn as get_valid_input,
 )
 
-_CACHE_STORE = None  # until I know valid singletons
 
+class BestBuyApp:
+    """The Best Buy application."""
 
-def _store(product_list: list | None = None) -> Store:
-    """Create a singleton Store instance.
+    def __init__(self, product_list: list[Product] | None = None) -> None:
+        """Init a new instance."""
+        self.store = Store(product_list)
 
-    If product_list is provided, initialized with those products.
-    """
-    global _CACHE_STORE  # noqa: PLW0603
-    if not _CACHE_STORE:
-        _CACHE_STORE = Store(product_list)
-    return _CACHE_STORE
+    def _place_order(self) -> None:
+        """Place an order for products."""
+        print("Available products:")
+        shopping_card = []
+        product_selection = None
+        amount_selection = None
+        available_products = self.store.get_all_products()
+        products_dispatch = {}
 
+        def _get_amount_in_cart(product: Product) -> int:
+            return sum([tup[1] for tup in shopping_card if tup[0] is product])
 
-def _place_order() -> None:
-    """Place an order for products."""
-    print("Available products:")
-    shopping_card = []
-    product_selection = None
-    amount_selection = None
-    available_products = _store().get_all_products()
-    products_dispatch = {}
+        def _should_abort() -> bool:
+            # Print abort msg if card is empty.
+            # Used in check on empty input
+            if not shopping_card:
+                print("\n" + ORDER_ABORT)
+                return True
+            return False
 
-    def _get_amount_in_cart(product: Product) -> int:
-        return sum([tup[1] for tup in shopping_card if tup[0] is product])
+        # construct and print product selection menu
+        for i in range(len(available_products)):
+            products_dispatch[i + 1] = available_products[i]
+            print(f"{i + 1}: ", end="")
+            available_products[i].show()
+        print("\n" + ORDER_EXIT_PROMPT + "\n")
 
-    def _should_abort() -> bool:
-        # Print abort msg if card is empty.
-        # Used in check on empty input
-        if not shopping_card:
-            print("\n" + ORDER_ABORT)
-            return True
-        return False
+        # loop ordering
+        while True:
+            new_product_selection = get_valid_input(
+                valid_inputs=[
+                    *list(range(1, len(products_dispatch) + 1)),
+                    "(Enter)",
+                ],
+                prompt=ORDER_PRODUCT_PROMPT,
+                exit_promt="",
+            )
 
-    # construct and print product selection menu
-    for i in range(len(available_products)):
-        products_dispatch[i + 1] = available_products[i]
-        print(f"{i + 1}: ", end="")
-        available_products[i].show()
-    print("\n" + ORDER_EXIT_PROMPT + "\n")
+            if not new_product_selection:
+                # returned from product selection menu wo selection
+                if _should_abort():
+                    # made not prev. placement, abort to main menu
+                    return
+                break
 
-    # loop ordering
-    while True:
-        new_product_selection = get_valid_input(
-            valid_inputs=[
-                *list(range(1, len(products_dispatch) + 1)),
-                "(Enter)",
-            ],
-            prompt=ORDER_PRODUCT_PROMPT,
-            exit_promt="",
-        )
+            product_selection = new_product_selection - 1  # reset from display
 
-        if not new_product_selection:
-            # returned from product selection menu wo selection
-            if _should_abort():
-                # made not prev. placement, abort to main menu
-                return
-            break
+            new_amount_selection = get_valid_input(
+                valid_inputs=[int, "(Enter)"],
+                prompt=ORDER_AMOUNT_PROMPT,
+                exit_promt="",
+            )
+            if not new_amount_selection:
+                # returned from amount selection menu wo selection
+                if _should_abort():
+                    # made not prev. placement, abort to main menu
+                    return
+                break
 
-        product_selection = new_product_selection - 1  # reset from display
-
-        new_amount_selection = get_valid_input(
-            valid_inputs=[int, "(Enter)"],
-            prompt=ORDER_AMOUNT_PROMPT,
-            exit_promt="",
-        )
-        if not new_amount_selection:
-            # returned from amount selection menu wo selection
-            if _should_abort():
-                # made not prev. placement, abort to main menu
-                return
-            break
-
-        items_of_product_availale = available_products[
-            product_selection
-        ].get_quantity() - _get_amount_in_cart(
-            available_products[product_selection],
-        )
-
-        if new_amount_selection > items_of_product_availale:
-            print(f"{ORDER_ERR_QUANT} {items_of_product_availale}")
-        else:
-            amount_selection = new_amount_selection
-
-            shopping_card.append((
+            items_of_product_availale = available_products[
+                product_selection
+            ].get_quantity() - _get_amount_in_cart(
                 available_products[product_selection],
-                amount_selection,
-            ))
+            )
 
-            print(ORDER_ADDED_TO_CART)
-            print()
+            if new_amount_selection > items_of_product_availale:
+                print(f"{ORDER_ERR_QUANT} {items_of_product_availale}")
+            else:
+                amount_selection = new_amount_selection
 
-    if product_selection is not None and amount_selection is not None:
-        print("\n\n***********")
-        total = _store().order(shopping_card)
-        print(ORDER_PLACED + str(total))
-        print("***********")
-    return
+                shopping_card.append((
+                    available_products[product_selection],
+                    amount_selection,
+                ))
 
+                print(ORDER_ADDED_TO_CART)
+                print()
 
-def _print_all_products() -> None:
-    """Print all products in the store."""
-    print("Products in store:")
-    all_products = _store().get_all_products()
-    if not all_products:
-        print(NO_PRODUCTS_MSG)
-    else:
-        for p in all_products:
-            p.show()
+        if product_selection is not None and amount_selection is not None:
+            print("\n\n***********")
+            total = self.store.order(shopping_card)
+            print(ORDER_PLACED + str(total))
+            print("***********")
+        return
 
+    def _print_all_products(self) -> None:
+        """Print all products in the store."""
+        print("Products in store:")
+        all_products = self.store.get_all_products()
+        if not all_products:
+            print(NO_PRODUCTS_MSG)
+        else:
+            for p in all_products:
+                p.show()
 
-def _get_total_store_stock() -> None:
-    """Print the total quantity of all products in the store."""
-    print(f"Total of {_store().get_total_quantity()} items in store")
+    def _get_total_store_stock(self) -> None:
+        """Print the total quantity of all products in the store."""
+        print(f"Total of {self.store.get_total_quantity()} items in store")
 
-
-def _start() -> None:
-    """Start the Best Buy application with a menu-driven interface."""
-    print()
-    print("Store Menu")
-    print("------")
-    menu_dispatch = {
-        1: ("List all products in store", _print_all_products),
-        2: ("Show total amount in store", _get_total_store_stock),
-        3: ("Make an order", _place_order),
-        4: ("Quit", sys.exit),
-    }
-    while True:
-        list(
-            map(
-                print,
-                (f"{tup[0]} {tup[1][0]}" for tup in menu_dispatch.items()),
-            ),
-        )  # unnecessary complex but fun
+    def start(self) -> None:
+        """Start the Best Buy application with a menu-driven interface."""
         print()
-        selection = get_valid_input(
-            valid_inputs=list(range(1, len(menu_dispatch) + 1)),
-            prompt=MENU_PROMPT(len(menu_dispatch)),
-        )
-
-        if not selection:  # exit by double enter none in get_valid_input
-            sys.exit()
-
-        print()
+        print("Store Menu")
         print("------")
-        menu_dispatch[selection][1]()
-        print()
+        menu_dispatch = {
+            1: ("List all products in store", self._print_all_products),
+            2: ("Show total amount in store", self._get_total_store_stock),
+            3: ("Make an order", self._place_order),
+            4: ("Quit", sys.exit),
+        }
+        while True:
+            list(
+                map(
+                    print,
+                    (f"{tup[0]} {tup[1][0]}" for tup in menu_dispatch.items()),
+                ),
+            )  # unnecessary complex but fun
+            print()
+            selection = get_valid_input(
+                valid_inputs=list(range(1, len(menu_dispatch) + 1)),
+                prompt=MENU_PROMPT(len(menu_dispatch)),
+            )
+
+            if not selection:  # exit by double enter none in get_valid_input
+                sys.exit()
+
+            print()
+            print("------")
+            menu_dispatch[selection][1]()
+            print()
 
 
 def init_superstore() -> None:
@@ -185,8 +176,7 @@ def init_superstore() -> None:
         Product("Bose QuietComfort Earbuds", price=250, quantity=500),
         Product("Google Pixel 7", price=500, quantity=250),
     ]
-    _store(product_list)
-    _start()
+    BestBuyApp(product_list).start()
 
 
 if __name__ == "__main__":
