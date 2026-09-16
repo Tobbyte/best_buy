222a90ff81e4e46f7d7b1f2f8072aeaf5d44a141
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-09-16 09:25:03 +0200

    fix line too long

--- products.py
+++ products.py
@@ -59,7 +59,8 @@ class Product:
     def show(self) -> None:
         """Print product details in a user-friendly format."""
         print(
-            f"'{self.name}', Price: {f'{self.price:.2f}'} ¤, Quantity: {self.quantity}",
+            f"'{self.name}', Price: {f'{self.price:.2f}'} ¤, "
+            "Quantity: {self.quantity}",
         )
 
     def buy(self, quantity: int) -> float:
